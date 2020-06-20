40c1557b1728a9a0f1a5bb0b59dc588a47bcaeea
Viktar Akulenka <repetitor202@gmail.com>
2020-06-20 20:02:32 +0300

    register-login-logout (axios)

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # js.html.tutorial
+
+##.env
+cp .env.example .env
+
+modify file .env if necessary
+
+##env.js
+cp env.example.js env.js
+
+modify file env.js if necessary
---
+++ env.example.js
@@ -0,0 +1,5 @@
+const JS_BACKEND_API = 'http://localhost:91/api';
+const JS_USER_NAME = 'jsname';
+const JS_USER_EMAIL = 'js@user.email';
+const JS_USER_PASSWORD = 'password';
+const JS_USER_TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9___';
